08f2896a162076f07dbcb1d8b8c06f5934ef1655
Ramon A. Garcia <ramon2garcia8@gmail.com>
2023-05-15 18:10:09 -0400

    array find method lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+function superbowlWin(array)  {
+    return array.find(isFound).year;
+}
+
+function isFound(element) {
+    let returnValue = ""
+    if (element.result === "W") {
+        returnValue = element.year
+    } else {
+        returnValue = undefined
+    }
+    return returnValue
+}
+  
+
